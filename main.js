df73880345133b6c034ce11d85d02245d916a927
Frank Aiello <frankvaiello@gmail.com>
2017-10-13 18:24:03 -0400

    Move fetch logic into main.js

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+import qs from 'qs';
+
+const BOOKS_URL = 'https://www.googleapis.com/books/v1/volumes';
+const BOOKS_KEY = process.env.BOOKS_KEY;
+
 // Renders an error message
 function showError(msg) {
   const html = `<li><p class="error">${msg}</p></li>`;
@@ -5,8 +10,21 @@ function showError(msg) {
 }
 
 // Searches for books and returns a promise that resolves a JSON list
-function searchForBooks(term) {
-  // TODO
+export function searchForBooks(term) {
+  const params = qs.stringify({ key: BOOKS_KEY, q: term })
+  const urlWithParams = `${BOOKS_URL}?${params}`
+  var request = new Request(urlWithParams, {
+      method: 'GET',
+      mode: 'cors',
+      headers: new Headers({
+        'Content-Type': 'text/plain'
+      })
+    }
+  );
+
+  return fetch(request).then(response => {
+    return response.json()
+  });
 }
 
 // Generate HTML and sets #results's contents to it
